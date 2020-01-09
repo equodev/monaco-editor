23d47ae049d1b7ad6cc97308a6c151dcc79a3cba
egrave <egrave@egrave-ltm>
2020-01-09 18:56:32 -0200

    Eclipse Monaco editor working without input

--- com.make.equo.eclipse.monaco.editor/src/com/make/equo/eclipse/monaco/editor/MonacoEditorPart.java
+++ com.make.equo.eclipse.monaco.editor/src/com/make/equo/eclipse/monaco/editor/MonacoEditorPart.java
@@ -1,76 +1,22 @@
 package com.make.equo.eclipse.monaco.editor;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IEditorSite;
-import org.eclipse.ui.IPropertyListener;
-import org.eclipse.ui.IWorkbenchPartSite;
 import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.part.EditorPart;
+import org.eclipse.ui.part.FileEditorInput;
 
-public class MonacoEditorPart implements IEditorPart {
+import com.make.swtcef.Chromium;
 
-	@Override
-	public void addPropertyListener(IPropertyListener listener) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void createPartControl(Composite parent) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public IWorkbenchPartSite getSite() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getTitle() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Image getTitleImage() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getTitleToolTip() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void removePropertyListener(IPropertyListener listener) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void setFocus() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public <T> T getAdapter(Class<T> adapter) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+public class MonacoEditorPart extends EditorPart {
 
 	@Override
 	public void doSave(IProgressMonitor monitor) {
@@ -81,6 +27,13 @@ public class MonacoEditorPart implements IEditorPart {
 	@Override
 	public void doSaveAs() {
 		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
+		setInput(input);
+		setSite(site);
 
 	}
 
@@ -97,25 +50,37 @@ public class MonacoEditorPart implements IEditorPart {
 	}
 
 	@Override
-	public boolean isSaveOnCloseNeeded() {
-		// TODO Auto-generated method stub
-		return false;
+	public void createPartControl(Composite parent) {
+		IEditorInput input = getEditorInput();
+		setPartName(input.getName());
+		if (input instanceof FileEditorInput) {
+			FileEditorInput fileInput = (FileEditorInput) input;
+			setTitleToolTip(fileInput.getPath().toString());
+
+			try {
+				InputStream contents = fileInput.getFile().getContents();
+				InputStreamReader isReader = new InputStreamReader(contents);
+				BufferedReader reader = new BufferedReader(isReader);
+				StringBuffer sb = new StringBuffer();
+				String str;
+				while ((str = reader.readLine()) != null) {
+					sb.append(str);
+				}
+				String textContent = sb.toString();
+				// con este textContent hay que abrir el editor
+				Chromium browser = new Chromium(parent, parent.getStyle());
+				browser.setUrl("equomonaco");
+			} catch (CoreException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
 	}
 
 	@Override
-	public IEditorInput getEditorInput() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public IEditorSite getEditorSite() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
+	public void setFocus() {
 		// TODO Auto-generated method stub
 
 	}
