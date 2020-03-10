689fd381edfd97fabf93c60a4e8ddb3e133b6e50
Elias Menon <eliasmenon@gmail.com>
2020-03-10 08:48:59 -0300

    Change editor implementation to use TypeScript and WebPack for easier
    dependency managing.

---
+++ com.make.equo.monaco/equo-monaco-editor/src/handler.ts
@@ -0,0 +1,163 @@
+import { listen, MessageConnection } from 'vscode-ws-jsonrpc';
+import {
+    MonacoLanguageClient, CloseAction, ErrorAction,
+    MonacoServices, createConnection
+} from 'monaco-languageclient';
+import normalizeUrl = require('normalize-url');
+import ReconnectingWebSocket = require('reconnecting-websocket');
+
+let lastSavedVersionId: number;
+let editor: monaco.editor.IStandaloneCodeEditor;
+let model: monaco.editor.ITextModel;
+
+// register Monaco languages
+monaco.languages.register({
+    id: 'json',
+    extensions: ['.json', '.bowerrc', '.jshintrc', '.jscsrc', '.eslintrc', '.babelrc'],
+    aliases: ['JSON', 'json'],
+    mimetypes: ['application/json'],
+});
+
+// @ts-ignore
+equo.on("_doCreateEditor", (values: { text: string; name: string; }) => {
+    model = monaco.editor.createModel(
+        values.text,
+        "json", // language
+        monaco.Uri.file(values.name) // uri
+    );
+
+    editor = monaco.editor.create(document.getElementById('container')!, {
+        model: model,
+        lightbulb: {
+            enabled: true
+        }
+    });
+
+    lastSavedVersionId = model.getAlternativeVersionId();
+
+    MonacoServices.install(editor);
+
+    editor.onDidChangeCursorSelection((e: any) => {
+        // @ts-ignore
+        equo.send("_selection", e.selection);
+    });
+});
+
+// @ts-ignore
+equo.send("_createEditor");
+
+// create the web socket
+const url = createUrl('/sampleServer')
+const webSocket = createWebSocket(url);
+// listen when the web socket is opened
+listen({
+    webSocket,
+    onConnection: connection => {
+        // create and start the language client
+        const languageClient = createLanguageClient(connection);
+        const disposable = languageClient.start();
+        connection.onClose(() => disposable.dispose());
+    }
+});
+
+function createLanguageClient(connection: MessageConnection): MonacoLanguageClient {
+    return new MonacoLanguageClient({
+        name: "Sample Language Client",
+        clientOptions: {
+            // use a language id as a document selector
+            documentSelector: ['json'],
+            // disable the default error handler
+            errorHandler: {
+                error: () => ErrorAction.Continue,
+                closed: () => CloseAction.DoNotRestart
+            }
+        },
+        // create a language client connection from the JSON RPC connection on demand
+        connectionProvider: {
+            get: (errorHandler, closeHandler) => {
+                return Promise.resolve(createConnection(connection, errorHandler, closeHandler));
+            }
+        }
+    });
+}
+
+function createUrl(path: string): string {
+    const protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+    return normalizeUrl(`${protocol}://${location.host}${location.pathname}${path}`);
+}
+
+function createWebSocket(url: string): WebSocket {
+    const socketOptions = {
+        maxReconnectionDelay: 10000,
+        minReconnectionDelay: 1000,
+        reconnectionDelayGrowFactor: 1.3,
+        connectionTimeout: 10000,
+        maxRetries: Infinity,
+        debug: false
+    };
+    return new ReconnectingWebSocket(url, [], socketOptions);
+}
+
+document.onkeydown = keydown;
+
+function keydown(evt: any) {
+    if (evt.ctrlKey && evt.keyCode == 83) {
+        // @ts-ignore
+        equo.send("_doSave");
+    }
+}
+
+// @ts-ignore
+equo.on("_getContents", () => {
+    // @ts-ignore
+    equo.send("_doGetContents", { contents: editor.getValue() });
+});
+
+function notifyDirty() {
+    if (lastSavedVersionId !== model.getAlternativeVersionId()) {
+        // @ts-ignore
+        equo.send("_isDirtyNotification", { isDirty: lastSavedVersionId !== model.getAlternativeVersionId() });
+    }
+}
+
+// @ts-ignore
+equo.on("_didSave", () => {
+    lastSavedVersionId = model.getAlternativeVersionId();
+    notifyDirty();
+});
+
+// @ts-ignore
+equo.on("_subscribeIsDirty", () => {
+    editor.onDidChangeModelContent(() => {
+        notifyDirty();
+    });
+});
+
+// @ts-ignore
+equo.on("_doCopy", () => {
+    editor.getAction("editor.action.clipboardCopyAction").run();
+});
+
+// @ts-ignore
+equo.on("_doCut", () => {
+    editor.getAction("editor.action.clipboardCutAction").run();
+});
+
+// @ts-ignore
+equo.on("_doFind", () => {
+    editor.getAction("actions.find").run();
+});
+
+// @ts-ignore
+equo.on("_doPaste", () => {
+    editor.focus();
+    // @ts-ignore
+    equo.send("_canPaste");
+});
+
+// @ts-ignore
+equo.on("_doSelectAll", () => {
+    editor.focus();
+    // @ts-ignore
+    equo.send("_canSelectAll");
+});
---
+++ com.make.equo.monaco/equo-monaco-editor/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const lib = path.resolve(__dirname, "../resources");
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+const common = {
+    entry: {
+        "main": path.resolve(lib, "main.js"),
+        "editor.worker": 'monaco-editor-core/esm/vs/editor/editor.worker.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: lib
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }]
+    },
+    target: 'web',
+    node: {
+        fs: 'empty',
+        child_process: 'empty',
+        net: 'empty',
+        crypto: 'empty'
+    },
+    resolve: {
+        alias: {
+            'vscode': require.resolve('monaco-languageclient/lib/vscode-compatibility')
+        }
+    }
+};
+
+module.exports = common;
