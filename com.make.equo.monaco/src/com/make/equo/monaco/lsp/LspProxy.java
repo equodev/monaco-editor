7dc37364ef6b589209e6131d4184d2f80880e03f
Leandro <leandroperezb@gmail.com>
2020-04-21 15:34:54 -0300

    Ws proxy integrated

--- com.make.equo.monaco/src/com/make/equo/monaco/lsp/LspProxy.java
+++ com.make.equo.monaco/src/com/make/equo/monaco/lsp/LspProxy.java
@@ -1,20 +1,48 @@
 package com.make.equo.monaco.lsp;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Collection;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.osgi.framework.FrameworkUtil;
 
 public class LspProxy {
-	private final String serversFile = "/home/leandro/Descargas/jsonrpc-ws-proxy/servers.yml";
+	private String serversFile = null;
+	private String proxyFile = null;
 	private boolean serverOn = false;
 	private Process process = null;
 	private int instancesUsingServer = 0;
 	private Map<String, Collection<String>> servers = new HashMap<>();
+
+	public LspProxy() {
+		File bundle;
+		try {
+			bundle = FileLocator.getBundleFile(FrameworkUtil.getBundle(this.getClass()));
+			proxyFile = extractServerFile(bundle.toString());
+			File fileForServer = File.createTempFile("serversLsp", ".yml");
+			fileForServer.deleteOnExit();
+			serversFile = fileForServer.toString();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	private String formatServerName(String name) {
 		return name.replace(" ", "");
@@ -25,12 +53,44 @@ public class LspProxy {
 		return this;
 	}
 
+	public static String extractServerFile(String jarFile) throws IOException {
+		Path tempDir = Files.createTempDirectory("lspserver");
+		JarFile jar = new JarFile(jarFile);
+		Enumeration<JarEntry> enumEntries = jar.entries();
+		while (enumEntries.hasMoreElements()) {
+			JarEntry file = (JarEntry) enumEntries.nextElement();
+			File f = new java.io.File(tempDir + java.io.File.separator + file.getName());
+			if (file.isDirectory()) { // if its a directory, create it
+				f.mkdir();
+				continue;
+			}
+			f.getParentFile().mkdirs();
+			f.createNewFile();
+			InputStream in = new BufferedInputStream(jar.getInputStream(file));
+			OutputStream out = new BufferedOutputStream(new FileOutputStream(f));
+			byte[] buffer = new byte[2048];
+			for (;;) {
+				int nBytes = in.read(buffer);
+				if (nBytes <= 0) {
+					break;
+				}
+				out.write(buffer, 0, nBytes);
+			}
+			out.flush();
+			out.close();
+			in.close();
+		}
+		jar.close();
+		markForDelete(tempDir.toFile());
+		return new File(tempDir.toString(), "server.js").toString();
+	}
+
 	public synchronized void startServer() {
 		saveServersInFile();
 		if (!serverOn) {
 			try {
-				ProcessBuilder processBuilder = new ProcessBuilder("node", "/home/leandro/Descargas/jsonrpc-ws-proxy/dist/server.js", 
-						"--port", "3000", "--languageServers", serversFile);
+				ProcessBuilder processBuilder = new ProcessBuilder("node", proxyFile, "--port", "3000",
+						"--languageServers", serversFile);
 				process = processBuilder.start();
 			} catch (IOException e) {
 				process = null;
@@ -62,6 +122,16 @@ public class LspProxy {
 			writer.close();
 		} catch (FileNotFoundException | UnsupportedEncodingException e) {
 			e.printStackTrace();
+		}
+	}
+
+	private static void markForDelete(File directoryToBeDeleted) {
+		directoryToBeDeleted.deleteOnExit();
+		File[] allContents = directoryToBeDeleted.listFiles();
+		if (allContents != null) {
+			for (File file : allContents) {
+				markForDelete(file);
+			}
 		}
 	}
 
